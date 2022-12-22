e12429976bf8c96604e3e914ce4ccfbc66553ada
WangDanmeng <wangdanmeng98@163.com>
2022-12-22 09:43:37 +0800

    使用TSX代替Template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+  const count = ref(0)
+  const onClick=()=>{
+    count.value += 1
+  }
+</script>
+
 <template>
   <div>
-    Hi
+    {{count}}
+  </div>
+  <div>
+    <button @click="onClick">+1</button>
   </div>
 </template>
+
