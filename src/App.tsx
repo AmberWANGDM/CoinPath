55d58253e08768ae0105d119567b21e99da40c84
WangDanmeng <wangdanmeng98@163.com>
2022-12-22 10:19:21 +0800

    引入Vue-Router4

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { defineComponent } from 'vue';
+import { RouterView } from 'vue-router'
+
+export const App = defineComponent({
+  setup: (props, context) => {
+    return () => <>
+      <header>导航
+        <ul>
+          <li>
+            <router-link to="/">Foo</router-link>
+          </li>
+          <li>
+            <router-link to="/about">Bar</router-link>
+          </li>
+        </ul>
+      </header>
+      <div>
+        <RouterView />
+      </div>
+    </>
+  }
+})
