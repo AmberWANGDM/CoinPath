6045312cea4f9c895fd46886a9486982e1d76961
WangDanmeng <wangdanmeng98@163.com>
2022-12-24 22:47:31 +0800

    feature:创建欢迎界面

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,8 +1,22 @@
+import { RouteRecordRaw } from "vue-router";
+import { First } from "../components/Welcome/first";
+import { Fourth } from "../components/Welcome/fourth";
+import { Second } from "../components/Welcome/second";
+import { Third } from "../components/Welcome/third";
 import { Bar } from "../views/Bar";
 import { Foo } from "../views/Foo";
+import { Welcome } from "../views/Welcome";
 
 
-export const routes = [
+export const routes: RouteRecordRaw[] = [
   { path: '/', component: Foo },
   { path: '/about', component: Bar },
+  {
+    path: '/welcome', component: Welcome, children: [
+      { path: '1', component: First },
+      { path: '2', component: Second },
+      { path: '3', component: Third },
+      { path: '4', component: Fourth }
+    ]
+  }
 ]
