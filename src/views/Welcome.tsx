cc38c922832438bd4ea35224a967c099c6f1d9b0
WangDanmeng <wangdanmeng98@163.com>
2023-01-10 12:04:53 +0800

    feat: 完成欢迎界面动画

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,5 +1,5 @@
-import { defineComponent } from 'vue';
-import { RouterView } from 'vue-router';
+import { defineComponent, Transition, VNode } from 'vue';
+import { RouteLocationNormalizedLoaded, RouterView } from 'vue-router';
 import s from './Welcome.module.scss'
 import logo from '../assets/icons/logo.svg'
 export const Welcome = defineComponent({
@@ -8,7 +8,21 @@ export const Welcome = defineComponent({
       <header >
         <img src={logo} />
       </header>
-      <main><RouterView name="main" /></main>
+      <main>
+        <RouterView name="main" >
+          {
+            ({ Component: Content, route }: { Component: VNode, route: RouteLocationNormalizedLoaded }) =>
+              <Transition
+                enterFromClass={s.slide_fade_enter_from}
+                enterActiveClass={s.slide_fade_enter_active}
+                leaveToClass={s.slide_fade_leave_to}
+                leaveFromClass={s.slide_fade_leave_from}
+              >
+                {Content}
+              </Transition>
+          }
+        </RouterView>
+      </main>
       <footer><RouterView name="footer" /></footer>
     </div>
     )
