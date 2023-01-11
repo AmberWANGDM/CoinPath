b738844d65d88e7ca21bfb7e5444f9a011b5df81
WangDanmeng <wangdanmeng98@163.com>
2023-01-11 23:07:01 +0800

    fix: iconName允许为空的bug

--- src/shared/FloatButton/FloatButton.tsx
+++ src/shared/FloatButton/FloatButton.tsx
@@ -1,12 +1,19 @@
 import { defineComponent, PropType } from 'vue';
-import { Icon } from '../Icon/icon';
+import { Icon, iconName } from '../Icon/icon';
 import s from './FloatButton.module.scss';
 
 export const FloatButton = defineComponent({
+  props: {
+    // 透传给Icon组件的name属性
+    iconName: {
+      type: String as PropType<iconName>,
+      required: true
+    }
+  },
   setup: (props, context) => {
     return () => (
       <div class={s.floatButton}>
-        <Icon name="add" class={s.icon} />
+        <Icon name={props.iconName} class={s.icon} />
       </div>
     )
   }
--- src/shared/Icon/icon.tsx
+++ src/shared/Icon/icon.tsx
@@ -1,9 +1,13 @@
 import { defineComponent, PropType } from 'vue';
 import s from './icon.module.scss';
 
+export type iconName = 'add' | 'logo' | 'welcome1' | 'welcome2' | 'welcome3'
 export const Icon = defineComponent({
   props: {
-    name: String as PropType<string>,
+    name: {
+      type: String as PropType<iconName>,
+      required: true
+    }
   },
   setup: (props, context) => {
     return () => (
