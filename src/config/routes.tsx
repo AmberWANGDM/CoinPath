a673485794cef085ef4761c4dd1de3b605a2cc95
WangDanmeng <wangdanmeng98@163.com>
2023-01-11 21:57:09 +0800

    feat: 创建 StartPage 和 Button

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -6,16 +6,17 @@ import { Third } from "../components/Welcome/Third";
 import { FirstActions } from "../components/Welcome/FirstActions";
 import { SecondActions } from "../components/Welcome/SecondActions";
 import { ThirdActions } from "../components/Welcome/ThirdActions";
-
-
+import { StartPage } from "../views/StartPage";
 
 export const routes: RouteRecordRaw[] = [
-  { path: '/', redirect: '/welcome' }, {
+  { path: '/', redirect: '/welcome' },
+  {
     path: '/welcome', component: Welcome, children: [
       { path: '', redirect: '/welcome/1' },
       { path: '1', name: "Welcome1", components: { main: First, footer: FirstActions }, },
       { path: '2', name: "Welcome2", components: { main: Second, footer: SecondActions }, },
       { path: '3', name: "Welcome3", components: { main: Third, footer: ThirdActions }, },
     ]
-  }
+  },
+  { path: '/start', component: StartPage },
 ]
---
+++ src/shared/Button/Button.tsx
@@ -0,0 +1,17 @@
+import { defineComponent } from "vue";
+import s from './Button.module.scss'
+
+// 定义组件的 Props 类型
+interface Props {
+  onClick: (e: MouseEvent) => void;
+}
+
+export const Button = defineComponent<Props>({
+  setup: (props, context) => {
+    return () => (
+      <button class={s.button}>
+        {context.slots.default?.()}
+      </button>
+    )
+  }
+})
