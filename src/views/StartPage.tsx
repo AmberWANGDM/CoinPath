5c90422d022d045117ec28c7c22fbc70f020e015
WangDanmeng <wangdanmeng98@163.com>
2023-01-12 10:13:29 +0800

    feat: 添加Navbar组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -3,6 +3,7 @@ import { Button } from '../shared/Button/Button';
 import { Center } from '../shared/Center/Center';
 import { FloatButton } from '../shared/FloatButton/FloatButton';
 import { Icon } from '../shared/Icon/Icon';
+import { Navbar } from '../shared/Navbar/Navbar';
 import s from './StartPage.module.scss';
 
 export const StartPage = defineComponent({
@@ -12,12 +13,18 @@ export const StartPage = defineComponent({
     };
     return () => (
       <div>
-        <nav>menu</nav>
+        <nav>
+          <Navbar>
+            {
+              { title: () => 'CoinPath', icon: () => <Icon name="menu" class={s.navIcon} /> }
+            }
+          </Navbar>
+        </nav>
         <Center class={s.start_center_wrapper}>
           <Icon name="startCenter" class={s.start_center} />
         </Center>
         <div class={s.button_wrapper}>
-          <Button class={s.button} onClick={onClick}>测试</Button>
+          <Button class={s.button} onClick={onClick}>开始记账</Button>
         </div>
         <FloatButton iconName='add' />
       </div>
