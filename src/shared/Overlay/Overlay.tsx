76bfffd3840a95803aab6d17a21b41b170b53027
WangDanmeng <wangdanmeng98@163.com>
2023-01-12 15:05:53 +0800

    style: 完善Overlay组件样式

--- src/shared/Overlay/Overlay.tsx
+++ src/shared/Overlay/Overlay.tsx
@@ -1,4 +1,5 @@
 import { defineComponent, PropType } from 'vue';
+import { RouterLink } from 'vue-router';
 import { Icon } from '../Icon/Icon';
 import s from './Overlay.module.scss';
 
@@ -9,31 +10,41 @@ export const Overlay = defineComponent({
     }
   },
   setup: (props, context) => {
+    // 点击遮罩层时，关闭菜单
     const close = () => {
       props.onClose?.()
     }
+    // 点击登录
+    const onClickSignIn = () => { }
+
     return () =>
       <>
         <div class={s.mask} onClick={close}>
         </div>
         <div class={s.overlay}>
-          <section>
+          <section class={s.currentUser}>
             <h2>未登录用户</h2>
-            <p>点击这里登录</p>
+            <p onClick={onClickSignIn}>点击这里登录</p>
           </section>
           <nav>
-            <ul>
+            <ul class={s.action_list}>
               <li>
-                <Icon name='charts' />
-                <span>统计图表</span>
+                <RouterLink to="/statistics" class={s.action}>
+                  <Icon name="charts" class={s.icon} />
+                  <span>统计图表</span>
+                </RouterLink>
               </li>
               <li>
-                <Icon name='export' />
-                <span>导出数据</span>
+                <RouterLink to="/export" class={s.action}>
+                  <Icon name="export" class={s.icon} />
+                  <span>导出数据</span>
+                </RouterLink>
               </li>
               <li>
-                <Icon name='notify' />
-                <span>记账提醒</span>
+                <RouterLink to="/notify" class={s.action}>
+                  <Icon name="notify" class={s.icon} />
+                  <span>记账提醒</span>
+                </RouterLink>
               </li>
             </ul>
           </nav>
