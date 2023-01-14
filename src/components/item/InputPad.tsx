5af549c8fce453b3e54965d4f16b0838e823f084
WangDanmeng <wangdanmeng98@163.com>
2023-01-14 12:44:55 +0800

    style: Inputpad布局

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -5,21 +5,19 @@ import { DatePicker, Popup } from 'vant';
 export const InputPad = defineComponent({
   setup: (props, context) => {
     const buttons = [
-      { text: '1', onClick: () => { } },
-      { text: '2', onClick: () => { } },
-      { text: '3', onClick: () => { } },
-      { text: '清空', onClick: () => { } },
-      { text: '4', onClick: () => { } },
-      { text: '5', onClick: () => { } },
-      { text: '6', onClick: () => { } },
-      { text: '+', onClick: () => { } },
-      { text: '7', onClick: () => { } },
-      { text: '8', onClick: () => { } },
-      { text: '9', onClick: () => { } },
-      { text: '-', onClick: () => { } },
-      { text: '.', onClick: () => { } },
-      { text: '0', onClick: () => { } },
+      { text: '1', onClick: () => { appendText(1) } },
+      { text: '2', onClick: () => { appendText(2) } },
+      { text: '3', onClick: () => { appendText(3) } },
+      { text: '4', onClick: () => { appendText(4) } },
+      { text: '5', onClick: () => { appendText(5) } },
+      { text: '6', onClick: () => { appendText(6) } },
+      { text: '7', onClick: () => { appendText(7) } },
+      { text: '8', onClick: () => { appendText(8) } },
+      { text: '9', onClick: () => { appendText(9) } },
+      { text: '0', onClick: () => { appendText(0) } },
+      { text: '.', onClick: () => { appendText('.') } },
       { text: 'Del', onClick: () => { } },
+      { text: '清空', onClick: () => { } },
       { text: '提交', onClick: () => { } }
     ]
 
@@ -31,24 +29,29 @@ export const InputPad = defineComponent({
     const refDate = ref<string[]>([year, month, day])
     const refDatePickerVisible = ref(false)
 
+    const refAmount = ref('')
+    const appendText = (n: string | number) => { refAmount.value += n.toString() }
+
     const showDatePicker = () => { refDatePickerVisible.value = true }
     const hideDatePicker = () => { refDatePickerVisible.value = false }
     const setDate = ({ selectedValues }: any) => { refDate.value = selectedValues; refDatePickerVisible.value = false }
 
     return () => (
       <>
-        <div class={s.dateAndAmount}>
-          <span class={s.date}>
-            <Icon name="date" class={s.icon} />
-            <span>
-              <span onClick={showDatePicker}>{refDate.value[0] + '年' + refDate.value[1] + '月' + refDate.value[2]
-                + '日'}</span>
-              <Popup v-model:show={refDatePickerVisible.value} position='bottom' >
-                <DatePicker modelValue={refDate.value} title="选择日期" onCancel={hideDatePicker} onConfirm={setDate} />
-              </Popup>
+        <div class={s.dateAndAmount_wrapper}>
+          <div class={s.dateAndAmount}>
+            <span class={s.date}>
+              <Icon name="date" class={s.icon} />
+              <span>
+                <span onClick={showDatePicker}>{refDate.value[0] + '年' + refDate.value[1] + '月' + refDate.value[2]
+                  + '日'}</span>
+                <Popup v-model:show={refDatePickerVisible.value} position='bottom' >
+                  <DatePicker modelValue={refDate.value} title="选择日期" onCancel={hideDatePicker} onConfirm={setDate} />
+                </Popup>
+              </span>
             </span>
-          </span>
-          <span class={s.amount}>199.12</span>
+            <span class={s.amount}>{refAmount.value}</span>
+          </div>
         </div>
         <div class={s.buttons}>
           {
