7dd4e9c2996eb34775a046f6fe54c877cd625a10
WangDanmeng <wangdanmeng98@163.com>
2023-01-18 21:09:37 +0800

    wip: navbar sticky，添加TagEdit删除按钮

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -1,17 +1,24 @@
 import { defineComponent, reactive } from 'vue';
 import { MainLayout } from '../../layouts/MainLayout';
+import { Button } from '../../shared/Button/Button';
 import { Icon } from '../../shared/Icon/Icon';
 import { TagForm } from './TagForm';
+import s from './Tag.module.scss';
 
 export const TagEdit = defineComponent({
   setup: (props, context) => {
     return () => (
       <MainLayout>{{
-        title: () => '新建标签',
+        title: () => '编辑标签',
         icon: () => <Icon name='back' onClick={() => { }} />,
-        default: () => (
-          <TagForm />
-        )
+        default: () =>
+          <>
+            <TagForm />
+            <div class={s.actions}>
+              <Button class={s.removeTags}>删除标签</Button>
+              <Button class={s.removeTagsAndItems}>删除标签和记账</Button>
+            </div>
+          </>
       }}</MainLayout>
     )
   }
