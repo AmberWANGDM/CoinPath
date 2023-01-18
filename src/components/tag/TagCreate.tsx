79566d1d4471398990a3dcdcb543f9b32c8efd3a
WangDanmeng <wangdanmeng98@163.com>
2023-01-18 20:58:31 +0800

    wip: 抽离TagForm组件

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,69 +1,16 @@
 import { defineComponent, reactive } from 'vue';
 import { MainLayout } from '../../layouts/MainLayout';
-import { Button } from '../../shared/Button/Button';
-import { EmojiSelect } from '../../shared/EmojiSelect/EmojiSelect';
 import { Icon } from '../../shared/Icon/Icon';
-import { Rules, validate } from '../../shared/validate';
-import s from './Tag.module.scss';
+
+import { TagForm } from './TagForm';
 export const TagCreate = defineComponent({
   setup: (props, context) => {
-    const formData = reactive({
-      name: '',
-      sign: ''
-    })
-    const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({})
-
-    const onSubmit = (e: Event) => {
-      e.preventDefault()
-      // ts 会提前自动推断 rules 的类型, 需要导出 Rules 类型
-      const rules: Rules<typeof formData> = [
-        { key: 'name', type: 'required', message: '必填' },
-        { key: 'name', type: 'pattern', regex: /^.{0,4}$/, message: '最多4个字符' },
-        { key: 'sign', type: 'required', message: '请选择符号' }
-      ]
-      // typescript 的 keyof 语法 https://www.typescriptlang.org/docs/handbook/2/keyof-types.html
-      Object.assign(errors, {
-        name: undefined,
-        sign: undefined
-      })
-      Object.assign(errors, validate(formData, rules))
-    }
-
     return () => (
       <MainLayout>{{
         title: () => '新建标签',
         icon: () => <Icon name='back' onClick={() => { }} />,
         default: () => (
-          <form class={s.form} onSubmit={onSubmit}>
-            <div class={s.formRow}>
-              <label class={s.formLabel}>
-                <span class={s.formItem_name}>标签名</span>
-                <div class={s.formItem_value}>
-                  <input class={[s.formItem, s.input]} v-model={formData.name}></input>
-                </div>
-                <div class={s.formItem_errorHint}>
-                  <span>{errors['name'] ? errors['name'][0] : ' '}</span>
-                </div>
-              </label>
-            </div>
-            <div class={s.formRow}>
-              <label class={s.formLabel}>
-                <span class={s.formItem_name}>符号</span><span>{formData.sign}</span>
-                <div class={s.formItem_value}>
-                  <EmojiSelect class={[s.formItem, s.emojiList]} v-model={formData.sign} />
-                </div>
-                <div class={s.formItem_errorHint}>
-                  <span>{errors['sign'] ? errors['sign'][0] : ' '}</span>
-                </div>
-              </label>
-            </div>
-            <p class={s.tips}>记账时长按标签即可进行编辑</p>
-            <div class={s.formRow}>
-              <div class={s.formItem_value}>
-                <Button class={[s.formItem, s.button]}>确定</Button>
-              </div>
-            </div>
-          </form>
+          <TagForm />
         )
       }}</MainLayout>
     )
