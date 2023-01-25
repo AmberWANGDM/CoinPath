2af3ca1294e422ea5eccab89ca54d53b97c386d9
WangDanmeng <wangdanmeng98@163.com>
2023-01-25 10:29:20 +0800

    feat: 检查是否跳过广告

--- src/components/welcome/SecondActions.tsx
+++ src/components/welcome/SecondActions.tsx
@@ -1,11 +1,12 @@
 import { FunctionalComponent } from 'vue'
 import { RouterLink } from 'vue-router'
+import { SkipFeatures } from '../../shared/SkipFeatures'
 import s from './welcome.module.scss'
 
 export const SecondActions: FunctionalComponent = () => {
   return <div class={s.actions}>
     <RouterLink class={s.next} to="/welcome/3">下一页</RouterLink>
-    <RouterLink class={s.jump} to="/start">跳过</RouterLink>
+    <SkipFeatures class={s.jump} />
   </div>
 }
 
--- src/components/welcome/ThirdActions.tsx
+++ src/components/welcome/ThirdActions.tsx
@@ -1,10 +1,12 @@
 import { FunctionalComponent } from 'vue'
 import { RouterLink } from 'vue-router'
 import s from './welcome.module.scss'
-
+const onClick = () => {
+  localStorage.setItem('skipFeatures', 'yes')
+}
 export const ThirdActions: FunctionalComponent = () => {
   return <div class={s.actions}>
-    <RouterLink class={s.next} to="/start">进入首页</RouterLink>
+    <span onClick={onClick}><RouterLink class={s.next} to="/start">进入首页</RouterLink></span>
   </div>
 }
 
