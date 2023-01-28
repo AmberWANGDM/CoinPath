3c15733fd2ba9ff327ae0480a1b30f8d8e6ab692
WangDanmeng <wangdanmeng98@163.com>
2023-01-28 18:54:34 +0800

    feat: 完善饼图

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -11,6 +11,8 @@ const DAY = 24 * 60 * 60 * 1000 // 一天的毫秒数为86400000
 
 type Data1Item = { happen_at: string, amount: number }
 type Data1 = Data1Item[]
+type Data2Item = { tag_id: number, tag: Tag, amount: number }
+type Data2 = Data2Item[]
 export const Charts = defineComponent({
   props: {
     startDate: {
@@ -31,20 +33,41 @@ export const Charts = defineComponent({
       const diff = new Date(props.endDate!).getTime() - new Date(props.startDate!).getTime()
       const n = diff / DAY + 1 // 两个日期之间的天数
       const array = Array.from({ length: n }).map((_, i) => {
-        const time = new Time(props.startDate + 'T00:00:00.000+0800').add(i, 'day').getTimeStamp()
+        const time = new Time(props.startDate).add(i, 'day').getTimeStamp()
         const amount = data1.value.find(item => new Date(item.happen_at).getTime() === time)?.amount || 0
         return [time, amount]
       })
       return array as [string, number][]
     })
+    // linechart data1
     onMounted(async () => {
       const response = await http.get<{ groups: Data1, summary: number }>('/items/summary', {
         happen_after: props.startDate!,
         happen_before: props.endDate!,
         kind: kind.value,
+        group_by: 'happen_at',
         _mock: 'itemSummary'
       })
       data1.value = response.data.groups
+    })
+    // piechart data2
+    const data2 = ref<Data2>([])
+    const betterData2 = computed<{ name: string; value: number }[]>(() =>
+      data2.value.map((item: Data2Item) => ({
+        name: item.tag.name,
+        value: item.amount
+      })
+      )
+    )
+    onMounted(async () => {
+      const response = await http.get<{ groups: Data2, summary: number }>('/items/summary', {
+        happen_after: props.startDate!,
+        happen_before: props.endDate!,
+        kind: kind.value,
+        group_by: 'tag_id',
+        _mock: 'itemSummary'
+      })
+      data2.value = response.data.groups
     })
     return () => (
       <>
@@ -60,7 +83,7 @@ export const Charts = defineComponent({
             v-model={kind.value}
           />
           <LineChart data={betterData1.value} />
-          <PieChart />
+          <PieChart data={betterData2.value} />
           <Bars />
         </div>
       </>
