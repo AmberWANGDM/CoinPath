1ab26c9082646a7cc7dbaffc4db9d1eadc78c895
WangDanmeng <wangdanmeng98@163.com>
2023-01-30 12:14:06 +0800

    fix: onMounted有未处理error的警告

--- src/hooks/useAfterMe.tsx
+++ src/hooks/useAfterMe.tsx
@@ -4,7 +4,19 @@ import { useMeStore } from "../stores/useMeStore"
 export const useAfterMe = (fn: () => void) => {
   const meStore = useMeStore()
   onMounted(async () => {
-    await meStore.mePromise
-    fn()
+    await meStore.mePromise!.then(fn, () => undefined)
+    // 1.0
+    // try {
+    //   await meStore.mePromise
+    // } catch (error) {
+    //   return
+    // }
+    // fn()
+    // 2.0
+    // const result = await meStore.mePromise!.catch(() => {
+    //   return new Error()
+    // })
+    // if (result instanceof Error) return
+    // fn()
   })
 }
