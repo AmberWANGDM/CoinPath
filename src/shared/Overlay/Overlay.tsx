b150593a73ff2f8ab4f19681e6b42041728fb326
WangDanmeng <wangdanmeng98@163.com>
2023-02-03 13:55:16 +0800

    :children_crossing: 侧边导航高亮

--- src/shared/Overlay/Overlay.tsx
+++ src/shared/Overlay/Overlay.tsx
@@ -1,6 +1,6 @@
 import { Dialog } from 'vant';
 import { defineComponent, onMounted, PropType, ref } from 'vue';
-import { RouterLink, useRoute, useRouter } from 'vue-router';
+import { RouterLink, useRoute, } from 'vue-router';
 import { useMeStore } from '../../stores/useMeStore';
 import { Icon } from '../Icon/Icon';
 import s from './Overlay.module.scss';
@@ -13,10 +13,11 @@ export const Overlay = defineComponent({
   },
   setup: (props, context) => {
     const route = useRoute()
-    const router = useRouter()
     const meStore = useMeStore()
     const me = ref<User>()
     onMounted(async () => {
+      const active = document.querySelector('.router-link-active')
+      active?.classList.add(s.active)
       const response = await meStore.mePromise
       me.value = response?.data.resource
     })
@@ -27,6 +28,8 @@ export const Overlay = defineComponent({
       }).then(() => {
         localStorage.removeItem('jwt')
         window.location.reload()
+      }).catch((e: MouseEvent) => {
+        props.onClose?.(e)
       })
     }
     return () =>
@@ -37,11 +40,11 @@ export const Overlay = defineComponent({
           <section class={s.currentUser}>
             {me.value ?
               <>
-                <RouterLink to={'/items'}><h2 class={s.email}>{me.value.email}</h2></RouterLink>
+                <h2 class={s.email}>{me.value.email}</h2>
                 <p onClick={onSignOut}>点击这里注销</p>
               </>
               :
-              <RouterLink to={`'/sign_in?return_to=${route.fullPath}`}>
+              <RouterLink to={`'/sign_in?return_to=${route.fullPath}&preview=yes`}>
                 <h2>未登录用户</h2>
                 <p>点击这里登录</p>
               </RouterLink>
@@ -49,25 +52,25 @@ export const Overlay = defineComponent({
           </section>
           <nav>
             <ul class={s.action_list}>
-              <li>
+              <li onClick={props.onClose}>
                 <RouterLink to="/items" class={s.action}>
                   <Icon name="home" class={s.icon} />
-                  <span>记账页面</span>
+                  <span >记账页面</span>
                 </RouterLink>
               </li>
-              <li>
+              <li onClick={props.onClose}>
                 <RouterLink to="/statistics" class={s.action}>
                   <Icon name="charts" class={s.icon} />
                   <span>统计图表</span>
                 </RouterLink>
               </li>
-              <li>
+              <li onClick={props.onClose}>
                 <RouterLink to="/export" class={s.action}>
                   <Icon name="export" class={s.icon} />
                   <span>导出数据</span>
                 </RouterLink>
               </li>
-              <li>
+              <li onClick={props.onClose}>
                 <RouterLink to="/notify" class={s.action}>
                   <Icon name="notify" class={s.icon} />
                   <span>记账提醒</span>
@@ -89,7 +92,7 @@ export const OverlayIcon = defineComponent({
     return () => <>
       <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
       {refOverlayVisible.value &&
-        <Overlay onClose={() => { refOverlayVisible.value = false }} />
+        <Overlay onClose={(e) => { refOverlayVisible.value = false }} />
       }
     </>
   }
